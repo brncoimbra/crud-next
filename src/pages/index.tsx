414c9b23d9745c0514acc9c2740b1439d71e4059
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-02-16 11:36:21 -0300

    create layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
-import TesteComp from "@/components/TesteComp";
+import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 30, "4"),
+  ];
+
   return (
     <>
       <div
         className={
-          "flex h-screen justify-center items-center bg-gradient-to-r from-purple-500  to-blue-600"
+          "flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white"
         }
       >
-        <TesteComp></TesteComp>
+        <Layout titulo='Cadastro Simples'>
+          <Tabela clientes={clientes}></Tabela>
+        </Layout>
       </div>
     </>
   );
