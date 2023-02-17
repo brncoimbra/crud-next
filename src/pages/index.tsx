d2fc8690c35186274da92d8d57f790c569f10384
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-02-17 20:02:56 -0300

    create button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "@/components/Botao";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
 import Cliente from "@/core/Cliente";
@@ -10,17 +11,36 @@ export default function Home() {
     new Cliente("Pedro", 30, "4"),
   ];
 
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir... ${cliente.nome}`);
+  }
+
+  const handleButton = () => {
+    console.log("clicou");
+  };
+
   return (
-    <>
-      <div
-        className={
-          "flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white"
-        }
-      >
-        <Layout titulo='Cadastro Simples'>
-          <Tabela clientes={clientes}></Tabela>
-        </Layout>
-      </div>
-    </>
+    <div
+      className={
+        "flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white"
+      }
+    >
+      <Layout titulo='Cadastro Simples'>
+        <div className='flex justify-end'>
+          <Botao cor='green' className='mb-4' onClick={handleButton}>
+            Novo Cliente
+          </Botao>
+        </div>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
+      </Layout>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // "./pages/**/*.{js,ts,jsx,tsx}",
-    // "./components/**/*.{js,ts,jsx,tsx}",
+  purge: {
+    content: [
+      //  "./src/pages/**/*.{js,ts,jsx,tsx}",
+      //  "./src/components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [/^bg-/, /^to-/, /^from-/],
+  },
   theme: {
     extend: {},
   },
