2844e19b7b8dea713befc262c36204fd658e7391
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-02-20 17:16:17 -0300

    update index

---
+++ src/backend/db/ColecaoCliente.ts
@@ -0,0 +1,36 @@
+import Cliente from "@/core/Cliente";
+import ClienteRepositorio from "@/core/ClienteRepositorio";
+import firebase from "../config";
+
+export default class ColecaoCliente implements ClienteRepositorio  {
+  
+  conversor = {
+    toFirestore(cliente: Cliente) {
+      return {
+        nome: cliente.nome,
+        idade: cliente.idade,
+      }
+    },
+    fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Cliente {
+      const dados = snapshot?.data(options)
+      return new Cliente(dados.nome, dados.idade, snapshot?.id)
+    }
+  }
+  
+  async salvar(cliente: Cliente): Promise<Cliente> {
+    return null
+  }
+
+  async excluir(cliente: Cliente): Promise<void> {
+    return null
+  }
+
+  async obterTodos(cliente: Cliente): Promise<Cliente[]> {
+    return null
+  }
+
+  colecao() {
+    return firebase.firestore().collection('clientes').withConverter(this.#conversor)
+  }
+
+}
